f6de7c906c00ea43b684fe022619d054f823d852
Victor Faurschou <mail@victorfaurschou.com>
2026-06-15 22:01:24 +0200

    Add Mod Menu integration

---
+++ src/client/java/com/victorfaurschou/fuzzycommandhistory/client/ModMenuIntegration.java
@@ -0,0 +1,11 @@
+package com.victorfaurschou.fuzzycommandhistory.client;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> null;
+	}
+}
