907684e204ec6552a0b4f4cc0c69c04ea43bfd5b
Victor Faurschou <mail@victorfaurschou.com>
2026-06-15 14:53:56 +0200

    Unarchived fabric generated template

---
+++ src/client/java/com/victorfaurschou/fuzzycommandhistory/client/FuzzyCommandHistoryClient.java
@@ -0,0 +1,10 @@
+package com.victorfaurschou.fuzzycommandhistory.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FuzzyCommandHistoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
